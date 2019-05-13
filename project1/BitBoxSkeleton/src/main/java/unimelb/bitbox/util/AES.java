521c2a7bd70b39fe9890397747783a3c8349784f
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2019-05-13 11:14:39 +1000

    Update AES.java

--- project1/BitBoxSkeleton/src/main/java/unimelb/bitbox/util/AES.java
+++ project1/BitBoxSkeleton/src/main/java/unimelb/bitbox/util/AES.java
@@ -13,8 +13,16 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * Select AES/ECB/PKCS5Padding to encode and decode. Part of codes refers from
+ * https://blog.csdn.net/jellyjiao2008/article/details/80475446
+ * 
+ * @author yuqiangz@student.unimelb.edu.au
+ *
+ */
 public class AES {
 	public static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+	/*
 	public static void main(String[] args) throws Exception {
 		byte[] aseKey = generateDesKey(128);
 		String text = "To be continue";
@@ -22,33 +30,48 @@ public class AES {
 		System.out.println("Encoded Text is :" + new String(encodedText));
 		byte[] decodedText = AesDecode(encodedText, aseKey);
 		System.out.println("Decoded Text is :" + new String(decodedText));
-	}
+	}*/
 
-	public static byte[] generateDesKey(int length) throws Exception {
-		// 实例化
+	/**
+	 * Generate AES Key.
+	 * @param length the length of generated AES key
+	 */
+	public static byte[] generateAESKey(int length) throws Exception {
 		KeyGenerator kgen = null;
-		kgen = KeyGenerator.getInstance("AES");
-		// 设置密钥长度
+		kgen = KeyGenerator.getInstance("AES", "BC");
 		kgen.init(length);
-		// 生成密钥
 		SecretKey skey = kgen.generateKey();
-		// 返回密钥的二进制编码
 		return skey.getEncoded();
 	}
 
-	public static byte[] AesEncode(String str, byte[] key) throws Exception {
+	/**
+	 * Use provided AES key to encode String.
+	 * @param content the string you want to encode.
+	 * @param key the key used to encode.
+	 * @return the byte format of encoded content.
+	 */
+	public static byte[] AESEncode(String content, byte[] key) throws Exception {
 		byte[] result = null;
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		Cipher cipher = Cipher.getInstance(ALGORITHM, "BC");
-		SecretKeySpec keySpec = new SecretKeySpec(key, "AES"); // 生成加密解密需要的Key
+		SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
 		cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-		result = cipher.doFinal(str.getBytes("UTF-8"));
+		result = cipher.doFinal(content.getBytes("UTF-8"));
 		return result;
 	}
 	
-	public static byte[] AesDecode(byte[] encodedContent, byte[] key) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+	/**
+	 * Use provided AES key to decode encoded content.
+	 * @param encodedContent encoded content you want to decode
+	 * @param key the key used to decode.
+	 * @return the byte format of decoded content.
+	 */
+	public static byte[] AESDecode(byte[] encodedContent, byte[] key) 
+		throws NoSuchAlgorithmException, NoSuchProviderException, 
+		NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, 
+		InvalidKeyException {
 		byte[] decodedContent = null;
-		SecretKeySpec keySpec = new SecretKeySpec(key, "AES"); // 生成加密解密需要的Key
+		SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
         Cipher out = Cipher.getInstance(ALGORITHM, "BC");    
         out.init(Cipher.DECRYPT_MODE, keySpec);
         decodedContent = out.doFinal(encodedContent);  
